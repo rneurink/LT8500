29d84f77a65a7d255df4d4e6bce61e3ab03890f1
rneurink <ruben.neurink@gmail.com>
2021-02-03 21:49:58 +0100

    Added packing/unpacking functions. Added frame functions and simplified the lib

--- src/LT8500.cpp
+++ src/LT8500.cpp
@@ -1,7 +1,24 @@
+/**
+ * @file LT8500.cpp
+ * @author Ruben Neurink-Sluiman (ruben.neurink@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include <Arduino.h>
 #include "LT8500.h"
 
+/**
+ * @brief Construct a new LT8500::LT8500 object
+ * 
+ * @param LDI_Blank LDI_Blank pin to latch data input and reset the chip
+ * @param SDO SDO is the equivalent of MISO, this is used to detect if the chip booted and to get the status from the chip
+ * @param N_OpenLED Optional. Triggers a status frame to be sent from the chip (can also be done using the self test command )
+ */
 LT8500::LT8500(int8_t LDI_Blank, int8_t SDO, int8_t N_OpenLED) {
     LT8500_LDIBLANK = LDI_Blank;
     LT8500_SDO = SDO;
@@ -11,6 +28,18 @@ LT8500::LT8500(int8_t LDI_Blank, int8_t SDO, int8_t N_OpenLED) {
     LT8500_SPISettings = SPISettings(5000000, MSBFIRST, SPI_MODE0);
 }
 
+/**
+ * @brief Destroy the LT8500::LT8500 object
+ * 
+ */
+LT8500::~LT8500() {
+    end();
+}
+
+/**
+ * @brief Initializes the pins and SPI bus and prepares the chip
+ * @note also disables the correction as this is enabled by default
+ */
 void LT8500::begin() {
 #if defined(__AVR_ATtinyxy7__)
     LDI_bit_mask = digitalPinToBitMask(LT8500_LDIBLANK);
@@ -34,50 +63,195 @@ void LT8500::begin() {
     // enable the PWM clock
 
     // Initialize the correction registers
-    sendFrame(Correction_buffer, CMD_CORRECTION);
+    sendCorrectionFrame();
     delay(1);
-    sendFrame(Correction_buffer, CMD_CORRECTION_TOGGLE); // Correction enabled by default, so disable it
+    toggleCorrection(); // Correction enabled by default, so disable it
     delay(1);
 
     // update frame
-    sendFrame(PWM_buffer, CMD_SYNC_UPDATE);
+    sendSyncFrame();
     delay(1);
 }
 
+/**
+ * @brief Closes the SPI bus
+ * 
+ */
 void LT8500::end() {
     SPI.end();
 }
 
+/**
+ * @brief Resets the chip
+ * 
+ */
 void LT8500::reset() {
     sendResetPulse();
 }
 
-void LT8500::enableOutput() {
-    // enable outputs
-    sendFrame(PWM_buffer, CMD_OUT_ENABLE);
-    delay(1);
+/**
+ * @brief Sets the PWM value of a channel
+ * 
+ * @param channel the channel to set the value of (from 0 to 47)
+ * @param pwm the PWM value to set in the channel (from 0 to 4095)
+ */
+void LT8500::setPWM(uint8_t channel, uint16_t pwm) {
+    // Using a packet buffer to deal with the 12 bits per pwm channel
+    // on an even channel (0, 2, 4 etc) it will take up the complete lower byte (lsB) and half of the upper byte (msB)
+    // as 12 bits is 1.5 times 8 bits this is used as a multiplier. (int will be rounded down)
+    // on an odd channel (1, 3, 5 etc) it will take up half of the lower byte and the complete upper byte
+    pack12to8(PWM_buffer, channel, pwm);
 }
 
-void LT8500::disableOutput() {
-    // disable outputs
-    sendFrame(PWM_buffer, CMD_OUT_DISABLE);
-    delay(1);
+/**
+ * @brief Sets the correction data of a channel
+ * 
+ * @param channel the channel to set the correction value of (from 0 to 47)
+ * @param correction the correction value to set in the channel (from 0 to 63)
+ */
+void LT8500::setCorrection(uint8_t channel, uint8_t correction) {
+    // Using a packet buffer to deal with the 6 bits of correction per channel
+    pack6to8(Correction_buffer, channel, correction);
 }
 
-bool LT8500::sendFrame(uint8_t *buffer, uint8_t command) {
-
+/**
+ * @brief Sends a synchronous update frame which will update the PWM registers on the start of the PWM period
+ * 
+ */
+void LT8500::sendSyncFrame() {
     SPI.beginTransaction(LT8500_SPISettings);
 
     for (int i = 71; i >= 0; i--) {
-        SPI.transfer(buffer[i]);
+        SPI.transfer(PWM_buffer[i]);
     }
-    SPI.transfer(command);
+    SPI.transfer(CMD_SYNC_UPDATE);
 
     sendLDIPulse();
 
     SPI.endTransaction();
 }
 
+/**
+ * @brief Sends an asynchronous update frame which will update the PWM registers immediatly after the LDI pulse
+ * 
+ */
+void LT8500::sendAsyncFrame() {
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(PWM_buffer[i]);
+    }
+    SPI.transfer(CMD_ASYNC_UPDATE);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Sends the correction frame
+ * 
+ */
+void LT8500::sendCorrectionFrame() {
+    for (int i = 0 ; i < 48; i++) {
+        //Repackage the correction buffer into the frame buffer as the correction frame is pretty much only sent 1 time
+        pack12to8(Frame_buffer, i, (unpack6to8(Correction_buffer, i) << 6));
+    }
+
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(Frame_buffer[i]);
+    }
+    SPI.transfer(CMD_CORRECTION);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Enables the PWM outputs of the chip
+ * 
+ */
+void LT8500::enableOutput() {
+    // enable outputs
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(0x00);
+    }
+    SPI.transfer(CMD_OUT_ENABLE);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Disables the PWM outputs of the chip
+ * 
+ */
+void LT8500::disableOutput() {
+    // disable outputs
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(0x00);
+    }
+    SPI.transfer(CMD_OUT_DISABLE);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Toggle the CRD (Correction register disable) bit
+ * 
+ */
+void LT8500::toggleCorrection() {
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(0x00);
+    }
+    SPI.transfer(CMD_CORRECTION_TOGGLE);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Toggle the PSH (Phase shift) bit
+ * 
+ */
+void LT8500::togglePhaseShift() {
+    SPI.beginTransaction(LT8500_SPISettings);
+
+    for (int i = 71; i >= 0; i--) {
+        SPI.transfer(0x00);
+    }
+    SPI.transfer(CMD_PHASE_TOGGLE);
+
+    sendLDIPulse();
+
+    SPI.endTransaction();
+}
+
+/**
+ * @brief Self test the chip TODO
+ * 
+ */
+void LT8500::selfTest() {
+    // NOT IMPLEMENTED YET
+}
+
+/**
+ * @brief Sends an latch data input pulse on the LDI_BLANK pin. This pulse should be between 8 and 5.000 ns
+ * 
+ */
 void LT8500::sendLDIPulse() {
 
 #if defined (__AVR_ATtinyxy7__)
@@ -91,6 +265,10 @@ void LT8500::sendLDIPulse() {
 
 }
 
+/**
+ * @brief Sends a reset pulse on the LDI_BLANK pin. This pulse should be above 50.000 ns
+ * 
+ */
 void LT8500::sendResetPulse() {
 
 #if defined (__AVR_ATtinyxy7__)
@@ -104,3 +282,103 @@ void LT8500::sendResetPulse() {
 #endif
 
 }
+
+#pragma region Packing_Unpacking_functions
+/**
+ * Functions to deal with packing and unpacking 12 and 6 bits into 8 bit arrays 
+ */
+
+/**
+ * @brief Packs a 12 bit value into an 8 bit array
+ * 
+ * @warning There is no safety and this assumes you dealt correctly with the array sizes!!
+ * @param array the array to pack into
+ * @param index the packed index of the value 
+ * @param value the value to set (be aware that it is 12 bits so 0..4095)
+ */
+void LT8500::pack12to8(uint8_t array[], uint8_t index, uint16_t value) {
+    if (index & 1) {
+        // Odd index, half first byte on low
+        array[index * 3 / 2] &= ~0xF0; // clear the upper half the lsB
+        array[index * 3 / 2] |= (0xF0 & (value << 4)); // push the 4 lower bits of the value into the upper 4 bits of the lsB
+        array[index * 3 / 2 + 1] = (value >> 4); // push the upper 8 bits of the value into the msB
+    } else {
+        array[index * 3 / 2] = (0xFF & value); // push the lower 8 bits into the lsB
+        array[index * 3 / 2 + 1] &= ~0x0F; // clear the lower half of the msB 
+        array[index * 3 / 2 + 1] |= (value >> 8); // push the 4 upper bits of the value into the lower 4 bits of the msB
+    }
+}
+
+/**
+ * @brief Unpacks a 12 bit value from an 8 bit array
+ * 
+ * @warning There is no safety and this assumes you dealt correctly with the array sizes!!
+ * @param array the array to unpack from
+ * @param index the packed index of the value
+ * @return uint16_t the 12 bit unpacked value related to the packed \p index
+ */
+uint16_t LT8500::unpack12to8(uint8_t array[], uint8_t index) {
+    uint16_t value =0;
+    if (index & 1) {
+        value = (array[index * 3 / 2] & 0xF0) >> 4;
+        value |= array[index * 3 / 2 + 1] << 4;
+    } else {
+        value = array[index * 3 / 2];
+        value |= (array[index * 3 / 2 + 1] & 0x0F) << 8;
+    }
+    return value;
+}
+
+/**
+ * @brief Packs a 6 bit value into an 8 bit array
+ * 
+ * @warning There is no safety and this assumes you dealt correctly with the array sizes!!
+ * @param array the array to pack into
+ * @param index the packed index of the value
+ * @param value the value to set (be aware that it is 6 bits so 0..63)
+ */
+void LT8500::pack6to8(uint8_t array[], uint8_t index, uint8_t value) {
+    if ((index & 3) == 3) {
+        array[index * 3 / 4] &= ~0xFC; // clear the upper 6 bits
+        array[index * 3 / 4] |= (0xFC & (value << 2)); // push the 6 bits of the value into the upper 6 bits of the byte
+    } else if ((index & 2) == 2) {
+        array[index * 3 / 4] &= ~0xF0; // clear the upper 4 bits of the lsB
+        array[index * 3 / 4] |= (0xF0 & (value << 4)); // push the lower 4 bits of the value into the lsB
+        array[index * 3 / 4 + 1] &= ~0x03; // clear the lower 2 bits of the msB
+        array[index * 3 / 4 + 1] |= (0x03 & (value >> 4)); // push the upper 4 bits of the value into the lower 4 bits of the msB
+    } else if (index & 1) {
+        array[index * 3 / 4] &= ~0xC0; // clear the upper 2 bits of the lsB
+        array[index * 3 / 4] |= (0xC0 & (value << 6)); // push the lower 2 bits of the value into the lsB
+        array[index * 3 / 4 + 1] &= ~0x0F; // clear the lower 4 bits of the msB
+        array[index * 3 / 4 + 1] |= (0x0F & (value >> 2)); // push the upper 4 bits of the value into the lower 4 bits of the msB
+    } else {
+        array[index * 3 / 4] &= ~0x3F; // clear the lower 6 bits
+        array[index * 3 / 4] |= (0x3F & value); // push the lower 6 bits into the byte
+    }
+}
+
+/**
+ * @brief Unpacks a 6 bit value from an 8 bit array
+ * 
+ * @warning There is no safety and this assumes you dealt correctly with the array sizes!!
+ * @param array the array to unpack from
+ * @param index the packed index of the value
+ * @return uint8_t the 6 bit unpacked value related to the packed \p index
+ */
+uint8_t LT8500::unpack6to8(uint8_t array[], uint8_t index) {
+    // inverse of pack6to8
+    uint8_t value = 0;
+    if ((index & 3) == 3) {
+        value = (array[index * 3/4] & 0xFC) >> 2;
+    } else if ((index & 2) == 2) {
+        value = (array[index * 3/4] & 0xF0) >> 4;
+        value |= (array[index * 3/4 + 1] & 0x03) << 4;
+    } else if (index & 1) {
+        value = (array[index * 3/4] & 0xC0) >> 6;
+        value |= (array[index * 3/4 + 1] & 0x0F) << 2;
+    } else {
+        value = array[index * 3/4] & 0x3F;
+    }
+    return value;
+}
+#pragma endregion Packing_Unpacking_functions
--- src/LT8500.h
+++ src/LT8500.h
@@ -7,12 +7,8 @@
  * 
  * @copyright Copyright (c) 2021
  * 
- */
-
-#ifndef __LT8500_H
-#define __LT8500_H
-
-/* 
+ * https://www.analog.com/media/en/technical-documentation/data-sheets/LT8500.pdf 
+ * 
  * LT8500 Description
  * 48 independent channel PWM generator
  * 12-bit resolution
@@ -22,22 +18,44 @@
  * 6-bit PWM correction
  * Up to 6.1kHz PWM frequency with PWMCK at 25MHz
  * Phase shift option to reduce input currents
-*/
+ * 
+ * 
+ * TODO:
+ * - add self test
+ * - add port manipulation for SAMD, ESP32 and Atmega328 to speed up the commands
+ */
+
+#ifndef __LT8500_H
+#define __LT8500_H
 
 #include <Arduino.h>
 #include <SPI.h>
 
-// CMD make sure not to send 0x8X
-#define CMD_SYNC_UPDATE 0x00
-#define CMD_ASYNC_UPDATE 0x10
-#define CMD_CORRECTION 0x20
-#define CMD_OUT_ENABLE 0x30
-#define CMD_OUT_DISABLE 0x40
-#define CMD_SELF_TEST 0x50
-#define CMD_PHASE_TOGGLE 0x60
-#define CMD_CORRECTION_TOGGLE 0x70
+// CMD make sure not to send 0x8X as it is a reserved command
+#define CMD_SYNC_UPDATE 0x00 // Update PWM’s Synchronously to PWM Period
+#define CMD_ASYNC_UPDATE 0x10 // Update PWM’s Asynchronously to PWM Period
+#define CMD_CORRECTION 0x20 // Set PWM Correction Factor 
+#define CMD_OUT_ENABLE 0x30 // Enable PWM Outputs
+#define CMD_OUT_DISABLE 0x40 // Disable (Drive Low) PWM Outputs
+#define CMD_SELF_TEST 0x50 // Initiates Self Test
+#define CMD_PHASE_TOGGLE 0x60 // Toggle 16-Channel Bank 120° Phase-Shift (PHS) 
+#define CMD_CORRECTION_TOGGLE 0x70 // Toggle Correction Disable Bit in Multiplier (CRD) 
 
-// pins needed: SDI SCKI PWMCLK LDI
+/**
+ * LT8500 power up:
+ *  1. Apply power and drive LDIBLANK low. SDO will go low when the POR de-asserts
+ *  2. Send a correction frame 
+ *  3. Send an update frame
+ *  4. Send an output enable frame
+ * 
+ * The correction is enabled by default
+ * The phase shifting is disabled by default
+ * 
+ * The correction value is applied as following:
+ * PWM(corrected) = PWM * (2/3) * ((Correction + 32)/64)
+ * This correction sets a multiplier of about 0.5 times to 1.5 times the PWM value
+ * 
+ */
 
 class LT8500 {
 // Variables
@@ -51,9 +69,9 @@ private:
     SPISettings LT8500_SPISettings;
 
     uint8_t PWM_buffer[72]; // 48 channels of 12 bits
-    uint8_t Correction_buffer[72];
+    uint8_t Correction_buffer[36]; // 48 channels of 6 bits
     
-    uint8_t Status_buffer[73];
+    uint8_t Frame_buffer[72];
 
 #if defined(__AVR_ATtinyxy7__)
     uint8_t LDI_bit_mask;
@@ -63,21 +81,40 @@ private:
 //Functions
 public:
     LT8500(int8_t LDI_Blank, int8_t SDO, int8_t N_OpenLED = -1);
+    ~LT8500();
 
     void begin();
     void end();
 
     void reset();
 
+    void setPWM(uint8_t channel, uint16_t pwm);
+    void setCorrection(uint8_t channel, uint8_t correction);
+
+    void sendSyncFrame();
+    void sendAsyncFrame();
+    void sendCorrectionFrame();
+
     void enableOutput();
     void disableOutput();
 
-//protected:
-//private:
+    void toggleCorrection();
+    void togglePhaseShift();
 
-    bool sendFrame(uint8_t *buffer, uint8_t command);
+    void selfTest();
+
+
+//protected:
+private:
     void sendLDIPulse();
     void sendResetPulse();
+
+    void pack12to8(uint8_t array[], uint8_t index, uint16_t value);
+    uint16_t unpack12to8(uint8_t array[], uint8_t index);
+
+    void pack6to8(uint8_t array[], uint8_t index, uint8_t value);
+    uint8_t unpack6to8(uint8_t array[], uint8_t index);
+
 };
 
 
