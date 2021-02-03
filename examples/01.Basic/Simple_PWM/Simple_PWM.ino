30698585658173c229249667ffbc11b44dd75205
rneurink <ruben.neurink@gmail.com>
2021-02-03 21:50:15 +0100

    Updated the example to comply with the new functions

--- examples/01.Basic/Simple_PWM/Simple_PWM.ino
+++ examples/01.Basic/Simple_PWM/Simple_PWM.ino
@@ -2,9 +2,6 @@
 
 LT8500 lt(5,19);
 
-uint8_t testBuffer[72];
-void array_set(uint8_t channel, uint16_t value);
-
 void setup() {
 #ifdef __AVR_ATtinyx17__
   // Clock output on PB5
@@ -27,40 +24,16 @@ void soft_reset() {
 }
 #endif
 
-int16_t i = 0;
-bool i_down = false;
-
 void loop() {
   // put your main code here, to run repeatedly:
-  array_set(0,i);
-  lt.sendFrame(testBuffer, CMD_SYNC_UPDATE);
-  if (i_down) {
-    i -= 8;
-    if (i <= 0) {
-      i = 0;
-      i_down = false;
-    }
-  } else {
-    i += 8;
-    if (i >= 4095) {
-      i = 4095;
-      i_down = true;
-    }
+  for (int i = 0; i < 4096; i+=8) {
+    lt.setPWM(0, i);
+    lt.sendSyncFrame();
+    delay(1);
   }
-  delay(1);
-}
-
-void array_set(uint8_t channel, uint16_t value)
-{
-    if (channel & 1)
-    {
-        // Odd channel, half first byte on low
-        testBuffer[channel*3/2] &= ~0xF0;
-        testBuffer[channel*3/2] |= (0xF0 & (value << 4));
-        testBuffer[channel*3/2 + 1] = (value >> 4);
-    } else {
-        testBuffer[channel*3/2] = (0xFF & value);
-        testBuffer[channel*3/2 + 1] &= ~0x0F;
-        testBuffer[channel*3/2 + 1] |= (value >> 8);
-    }
+  for (int i = 4095; i >= 0; i-=8) {
+    lt.setPWM(0,i);
+    lt.sendSyncFrame();
+    delay(1);
+  }
 }
